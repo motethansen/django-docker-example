8d2889a7146b935231d4dba40c16c338c9d53ca2
Michael Hansen <motethansen@gmail.com>
2022-05-18 17:59:16 +0800

    fix settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/static'
+STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
-STATIC_ROOT = '/vol/web/static'
-MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static/'
+MEDIA_ROOT = '/vol/web/media/'
